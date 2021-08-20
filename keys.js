1ee5f89464ee2af2ff157dc58a5d5f63b9c276a2
Bhaskar-Newase-Ext <62342525+Bhaskar-Newase-Ext@users.noreply.github.com>
2021-08-20 22:00:37 +0530

    all changes commit at once

---
+++ keys.js
@@ -0,0 +1,5 @@
+// API KEY - https://developers.google.com/custom-search/v1/using_rest
+export const API_KEY = 'AIzaSyBFZJga39-nDmvlGIwCLq2zSYsvYLxqzBU'
+
+// Context key - https:/cse.google.com/cse/create/new
+export const CONTEXT_KEY = '73e3a5bba869faa5b'
